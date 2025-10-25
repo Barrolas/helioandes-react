d41d6ca01e07a3026c8152ce18ea6bf2d2b1ccac
NICOLE <ni.chavezg@duocuc.cl>
2025-10-24 22:04:18 -0300

    [ FEAT ] : se agregan estilos al navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavbarInicio = () => {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="bg-body-tertiary shadow-sm" sticky='top'>
             <Container>
-                <Navbar.Brand href="Inicio">React-Bootstrap</Navbar.Brand>
+            
+                <Navbar.Brand href="Inicio " className="d-flex align-items-center"><img src="/helioandes-logo.png" alt="HelioAndes Logo" style={{height: '30px', marginRight: '10px'}} /> HelioAndes</Navbar.Brand>   
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-auto">
                         <Nav.Link href="Inicio">Inicio</Nav.Link>
                         <Nav.Link href="Servicios">Servicios</Nav.Link>
-                        <Nav.Link href="DEMO">Demo</Nav.Link>
+                        <Nav.Link href="Soluciones">Soluciones</Nav.Link>
+                        <Nav.Link href="DEMO">DEMO</Nav.Link>
                         <Nav.Link href="Planes">Planes</Nav.Link>
                         <Nav.Link href="Testimonios">Testimonios</Nav.Link>
                         <Nav.Link href="FAQ">FAQ</Nav.Link>
