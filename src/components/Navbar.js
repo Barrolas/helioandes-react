1db61270e029047a9e8ddb0e1f1a1df79ef5e512
Barrolas <barrab.nicolas@gmail.com>
2025-10-24 21:28:07 -0300

    [ FIX ]: Se corrigen los nombres de import de elementos de bootstrap (Navbar, Hero y Footer).

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Navbar = () => {
+const NavbarInicio = () => {
     return (
         <div>
             <h1>Navbar</h1>
@@ -8,4 +8,4 @@ const Navbar = () => {
     );
 };
 
-export default Navbar;
+export default NavbarInicio;
