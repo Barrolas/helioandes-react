89a5455500b6a58becf0d48a2447c2d496f443a9
NICOLE <ni.chavezg@duocuc.cl>
2025-10-27 15:40:33 -0300

    [FEAT ]: footer actualizado

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+
+// Clases principales:
+// - bg-dark (fondo)
+// - text-white (texto)
+// - py-3 (espaciado)
+// - text-start y text-end (alineación)
+// - text-decoration-underline (subrayado)
 
 const FooterInicio = () => {
     return (
-        <Container id="footer">
-            <h1>Footer</h1>
-        </Container>
+        <footer className="bg-dark text-white py-3">
+            <Container>
+                <Row className="align-items-center">
+                    <Col className="text-start">
+                        <p className="mb-0">© 2025 HelioAndes Energía</p>
+                    </Col>
+                    
+                    <Col className="text-end">
+                        <Nav>
+                            <Nav.Link href="/privacidad" className="text-white text-decoration-underline">
+                                Privacidad
+                            </Nav.Link>
+                            <span className="mx-2">•</span>
+                            <Nav.Link href="/terminos" className="text-white text-decoration-underline">
+                                Términos
+                            </Nav.Link>
+                        </Nav>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     );
 };
 
